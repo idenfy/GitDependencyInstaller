394169c1f1bd9fabb662e269dda69125a5b8a593
Deividas Tamkus <deividas@idenfy.com>
2021-02-23 13:20:53 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='git_dependency_installer',
+    version='1.0.0',
+    license='GNU GENERAL PUBLIC LICENSE Version 3',
+    packages=find_packages(),
+    description='SDK that helps to manage dependencies directly from git repositories.',
+    include_package_data=True,
+    install_requires=[],
+    author='Deividas Tamkus',
+    author_email='deividas@idenfy.com',
+    keywords='Git Python Dependency Manager',
+    url='https://github.com/idenfy/GitDependencyInstaller.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+        'Operating System :: OS Independent',
+    ],
+)
